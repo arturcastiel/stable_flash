7562b393bfd554a998e3366bd5875244222bf643
ziliang96 <nhziliang@gmail.com>
2023-12-11 18:22:29 +0100

    aj bj computed for aqueous componets

--- testes.m
+++ testes.m
@@ -1,7 +1,6 @@
-
+addpath(genpath(pwd));
 %% Previous on how we are going to create the new Stability and Flash
-
-Components_list = [ComponentMethane, ComponentCarbonDioxide, ComponentWater];
+Components_list = [ComponentCarbonDioxide, ComponentMethane, ComponentWater];
 
 X_frac = [ 1 0 0; ...
            0 1 0; ...
